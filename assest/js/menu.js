7a9ff30b1f148fd3c9058a1f1819d9782c423b6a
ViniciusDeveloper <125139361+vihfrontend@users.noreply.github.com>
2023-04-12 06:13:12 -0300

    atualizei

--- assest/js/menu.js
+++ assest/js/menu.js
@@ -1,8 +1,13 @@
-(()=>{
- let menu = document.querySelector('.menu_bar');
- let openMenu = ()=>{
-    let listMenu = document.querySelector('.list_itens')
-    listMenu.classList.toggle('open_menu');
- }
- menu.addEventListener('click',openMenu)
-})()
+(() => {
+   // Selecionar o elemento do menu
+   let menu = document.querySelector('.menu_bar');
+ 
+   // Função para abrir/fechar o menu
+   let openMenu = () => {
+     let listMenu = document.querySelector('.list_itens');
+     listMenu.classList.toggle('open_menu');
+   };
+ 
+   // Adicionar um evento de clique ao elemento do menu
+   menu.addEventListener('click', openMenu);
+ })();
